85a6cfdfbced3f3cd19ad85bb32f1ed2cd97b02b
Maxim Polanov <maxdemented372@gmail.com>
2025-09-02 07:18:43 +0300

    spec for 3 september on email page

--- dist/js/email-marketing.js
+++ dist/js/email-marketing.js
@@ -98,19 +98,77 @@ if (document.querySelector('.results .swiper')) {
 	});
 }
 
-document.querySelectorAll('.my-page')[1].style.backgroundColor = 'white';
+// document.querySelectorAll('.my-page')[1].style.backgroundColor = 'white';
 
-const pageFlip = new St.PageFlip(document.getElementById('book'), {
-	width: window.innerWidth,
-	height: window.innerHeight,
-	showCover: false,
-	size: 'stretch',
-	// drawShadow: false,
-	maxShadowOpacity: 0.5, // Half shadow intensity
-});
+// const pageFlip = new St.PageFlip(document.getElementById('book'), {
+// 	width: window.innerWidth,
+// 	height: window.innerHeight,
+// 	showCover: false,
+// 	size: 'stretch',
+// 	// drawShadow: false,
+// 	maxShadowOpacity: 0.5, // Half shadow intensity
+// });
 
-pageFlip.loadFromHTML(document.querySelectorAll('.my-page'));
+// pageFlip.loadFromHTML(document.querySelectorAll('.my-page'));
+const button = document.getElementById('show-date');
+const wrapper = document.querySelector('.page-wrap');
 
-document.querySelector('#test').addEventListener('click', () => {
-	pageFlip.flipNext({ corner: 'top' });
-});
+if (document.querySelector('.karaoke-date__item')) {
+	const items = document.querySelectorAll('.karaoke-date__item');
+	const button = document.querySelector('#show-date');
+
+	button.addEventListener('click', () => {
+		button.style.visibility = 'hidden';
+		// Сначала вращаем
+		wrapper.classList.add('show-rotate');
+
+		// Через 0.6–0.8 секунды сдвигаем лендинг
+		setTimeout(() => {
+			wrapper.classList.add('show-date');
+		}, 300);
+		setInterval(createLeaf, 700);
+		for (let i = 0; i < 10; i++) {
+			createLeaf();
+		}
+		setTimeout(() => {
+			const secondDate = document.querySelector('.calendar-date_second');
+			secondDate.classList.add('calendar-date_second-out');
+			items.forEach((item, i) => {
+				setTimeout(() => {
+					if (i != 0 && i != items.length) {
+						items[i - 1].classList.remove('karaoke-date__item_active');
+					}
+					item.style.opacity = 1;
+					item.classList.add('karaoke-date__item_active');
+					if (i === items.length - 1) {
+						document.querySelector('.date-page__button').style.opacity = 1;
+					}
+				}, i * 3000);
+			});
+		}, 2600);
+	});
+}
+function createLeaf() {
+	const leaf = document.createElement('div');
+	leaf.classList.add('leaf');
+
+	const startLeft = Math.random() * window.innerWidth;
+	const duration = 5 + Math.random() * 5;
+	const xMove = (Math.random() - 0.5) * 200 + 'px';
+
+	// случайные оттенки
+	const hue = Math.floor(Math.random() * 90 - 45) + 'deg'; // -45..45 градусов
+	const bright = (0.8 + Math.random() * 0.4).toFixed(2); // 0.8–1.2
+	const sat = (0.7 + Math.random() * 0.6).toFixed(2); // 0.7–1.3
+
+	leaf.style.left = startLeft + 'px';
+	leaf.style.setProperty('--x-move', xMove);
+	leaf.style.animationDuration = duration + 's';
+	leaf.style.setProperty('--hue', hue);
+	leaf.style.setProperty('--bright', bright);
+	leaf.style.setProperty('--sat', sat);
+
+	document.querySelector('.date-page').appendChild(leaf);
+
+	setTimeout(() => leaf.remove(), duration * 1000);
+}
