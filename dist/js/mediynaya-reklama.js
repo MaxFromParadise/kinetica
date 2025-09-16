93134878694c540da52069ec6a4ada3ccfad15c1
Maxim Polanov <maxdemented372@gmail.com>
2025-09-16 14:30:17 +0300

    mediynaya start

---
+++ dist/js/mediynaya-reklama.js
@@ -0,0 +1,21 @@
+// top-awards
+if (document.querySelector('.top-awards__slider .swiper')) {
+	const topAwardsSwiper = new Swiper('.top-awards__slider .swiper', {
+		// Optional parameters
+		loop: false,
+		freeMode: true,
+		slidesPerView: 'auto',
+		spaceBetween: 60,
+
+		navigation: {
+			nextEl: '.top-awards__next',
+			prevEl: '.top-awards__prev',
+		},
+		breakpoints: {
+			// when window width is >= 992px
+			992: {
+				spaceBetween: 150,
+			},
+		},
+	});
+}
